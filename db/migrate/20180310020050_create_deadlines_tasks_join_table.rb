2de2f545f3f4be63552750f8ee775dc3dd153eb7
Jordan Mynes <mynesjordan@gmail.com>
2018-03-09 22:38:03 -0600

    Removing many-to-many relation   $ rails destroy migration CreateDeadlinesTasksJoinTable

--- db/migrate/20180310020050_create_deadlines_tasks_join_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateDeadlinesTasksJoinTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :deadlines_tasks, id: false do |t|
-      t.belongs_to :deadline, index: true
-      t.belongs_to :task, index: true
-    end
-  end
-end
-
