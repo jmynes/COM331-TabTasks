3e30eae41b814a6851c2c1ff2903fec29c327eae
Jordan Mynes <mynesjordan@gmail.com>
2018-03-09 20:00:40 -0600

    Run Deadline migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310015848) do
+ActiveRecord::Schema.define(version: 20180310020010) do
+
+  create_table "deadlines", force: :cascade do |t|
+    t.string "timeframe"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "verb"
