3851bd761d443bbfe264becc5166b2a550dab648
Jordan Mynes <mynesjordan@gmail.com>
2018-03-09 20:03:13 -0600

    Write migration to create deadlines-tasks join table

--- db/migrate/20180310020050_create_deadlines_tasks_join_table.rb
+++ db/migrate/20180310020050_create_deadlines_tasks_join_table.rb
@@ -1,4 +1,8 @@
 class CreateDeadlinesTasksJoinTable < ActiveRecord::Migration[5.1]
   def change
+    create_table :deadlines_tasks, id: false do |t|
+    t.belongs_to :deadline, index: true
+    t.belongs_to :task, index: true
   end
 end
+
