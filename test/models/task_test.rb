c1c94df98b3e7da207b854ea884af55c048caed3
Jordan Mynes <mynesjordan@gmail.com>
2018-03-10 18:09:09 -0600

    Fixed whitespace errors

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-     test "should not save task without a verb" do
-     task = Task.new
-     assert_not task.save, "Saved a task without a verb"
+  test "should not save task without a verb" do
+    task = Task.new
+    assert_not task.save, "Saved a task without a verb"
   end
 end
-
