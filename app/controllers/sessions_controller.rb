32dd7f66c6ab2540e4faef5aa472dca683f230a8
Jordan Mynes <mynesjordan@gmail.com>
2018-04-11 00:55:25 -0500

    Create user from hash; reflect in diagnostic view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   def create
-    @provider_response = request.env['omniauth.auth']
+    auth_hash = request.env['omniauth.auth']
+    @user = User.find_or_create_by(uid: auth_hash['uid']) do |user|
+      user.nickname = auth_hash['info']['nickname']
+      user.email = auth_hash['info']['email']
+    end
   end
 end
