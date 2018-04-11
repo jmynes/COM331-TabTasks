7c4e793375855c8780d9e712f210ff4e61277fea
Jordan Mynes <mynesjordan@gmail.com>
2018-04-11 17:24:03 -0500

    Fixed login redirect url, login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       user.email = auth_hash['info']['email']
     end
     self.current_user = @user
-    redirect_to posts_url, notice: "You have logged in, welcome: #{current_user.nickname}!"
+    redirect_to tasks_url, notice: "You have logged in, welcome: #{current_user.nickname}!"
   end
   def login
   end
