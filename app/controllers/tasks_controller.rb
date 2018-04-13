51b16a8fa9bbf0dd19fb356b7e922bc7f0e8dd05
Jordan Mynes <mynesjordan@gmail.com>
2018-04-12 22:00:38 -0500

    Commenting out user variable from task creation until model is updated

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
-    @task.user = current_user
+    # @task.user = current_user
 
     respond_to do |format|
       if @task.save
