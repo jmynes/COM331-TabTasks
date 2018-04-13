4865cca77fd7fa7b5ceddb9fb327a6f830db9d78
Haris Uddin <harisghias94.hg@gmail.com>
2018-04-12 23:21:55 -0500

    Foreign key relation added between user and task    rails g migration AddUserToTask user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411055028) do
+ActiveRecord::Schema.define(version: 20180413041153) do
 
   create_table "deadlines", force: :cascade do |t|
     t.string "timeframe"
@@ -24,7 +24,9 @@ ActiveRecord::Schema.define(version: 20180411055028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "deadline_id"
+    t.integer "user_id"
     t.index ["deadline_id"], name: "index_tasks_on_deadline_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
